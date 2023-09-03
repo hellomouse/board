aec46b9a56134218d736fb7ee533ca5bdc2dc8c3
Bowserinator <bowserinator@gmail.com>
2023-09-03 17:21:22 -0400

    Add API doc for site status

--- pages/docs/site/index.vue
+++ pages/docs/site/index.vue
@@ -31,6 +31,45 @@
                 <b>Error:</b> 401 Unauthorized, 500 Server error<br>
             </p>
         </ApiDoc>
+
+        <ApiDoc
+            title="Get status of jobs"
+            method="GET"
+            endpoint="/api/site/status"
+            param-type="URLParams"
+            :auth="true"
+        >
+            Get status of all download tasks for the currently logged in user
+
+            <v-table density="compact" class="api-parameter-table my-4">
+                <thead>
+                    <tr>
+                        <th class="text-left">Parameter</th>
+                        <th class="text-left">Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>offset: 0?</td><td>Offset to get.</td></tr>
+                    <tr><td>limit: 100?</td><td>Limit of jobs to get, at most 100</td></tr>
+                </tbody>
+            </v-table>
+
+            <p>
+                <b>Success:</b> 200 OK
+                <pre class="api-json-block"><code>{ jobs: [{
+    id: uuid of job,
+    created: date created,
+    finished: date completed, or same as created if not finished,
+    name: command, ie "preview_pin",
+    data: data string passed to command,
+    requestor: id of user who initiated the task,
+    priority: priority of task, bigger number = higher,
+    status: status, "completed|errored|processing|queued"
+}, ...]}</code></pre><br>
+
+                <b>Error:</b> 401 Unauthorized, 500 Server error<br>
+            </p>
+        </ApiDoc>
     </div>
 </template>
 
