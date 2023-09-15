4826b69287aad2402a26edf25d2287a781371171
Bowserinator <bowserinator@gmail.com>
2023-09-14 23:28:40 -0400

    Preserve query when redirecting to login

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,10 @@ import { useAuthStore } from "~/store/auth.js";
 // eslint-disable-next-line no-undef
 export default defineNuxtRouteMiddleware((to, from) => {
     const authStore = useAuthStore();
-    if (!authStore.isLoggedIn && to.path !== '/login')
+    if (!authStore.isLoggedIn && to.path !== '/login') {
+        let query = to.query ? ('?' + new URLSearchParams(to.query).toString()) : '';
+
         // eslint-disable-next-line no-undef
-        return navigateTo('/login?r=' + to.path);
+        return navigateTo('/login?r=' + to.path + query);
+    }
 });
